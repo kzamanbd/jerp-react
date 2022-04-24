533bf2491a4c7efdd8c98a17222ea9e20759bf0f
Kamruzzaman <zaman7u@gmail.com>
2022-04-24 21:49:27 +0600

    update app router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,59 @@
-import AppRouter from './navigation/AppRouter';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PrivateRoute from './utils/PrivateRoute';
+import PublicRoute from './utils/PublicRoute';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Login from './pages/Auth/Login';
+import PageNotFound from './pages/PageNotFound';
+import OrderCreate from './pages/Orders/Create/OrderCreate';
 
-export default function JerpApp() {
-    return <AppRouter />;
+function JerpApp() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <PrivateRoute>
+                            <Dashboard />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/login"
+                    element={
+                        <PublicRoute>
+                            <Login />
+                        </PublicRoute>
+                    }
+                />
+                <Route
+                    path="/features/users/dashboard"
+                    element={
+                        <PrivateRoute>
+                            <Dashboard />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/features/local_sales/sales_order"
+                    element={
+                        <PrivateRoute>
+                            <OrderCreate />
+                        </PrivateRoute>
+                    }
+                />
+                {/* page not found */}
+                <Route
+                    path="*"
+                    element={
+                        <PrivateRoute>
+                            <PageNotFound />
+                        </PrivateRoute>
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
+    );
 }
+
+export default JerpApp;
---
+++ src/pages/Orders/Create/OrderCreate.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function OrderCreate() {
+    return <div>OrderCreate</div>;
+}
+
+export default OrderCreate;
